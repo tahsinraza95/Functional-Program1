9fc68ff0886b84140802379e916f0301bae4730b
tahsinraza95 <tahsinr693@gmail.com>
2023-02-06 05:05:14 +0530

    Updated: Value

---
+++ Quotient and Remainder/Quotient and Remainder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Quotient_and_Remainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int divided = 50, divisor = 8;
+            int quotient = divided / divisor;
+            int remainder = divided % divisor;
+            Console.WriteLine("Dividend:{0} Divisor:{1}",divided,divisor);
+            Console.WriteLine("Quotient = + quotient");
+            Console.WriteLine("Remainder = + remainder");
+            Console.WriteLine();
+        }
+    }
+}
